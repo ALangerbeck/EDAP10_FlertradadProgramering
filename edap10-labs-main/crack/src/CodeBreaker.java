69ad6e6fbdda73714f70542b10e06a26f82bd9d9
Alfred Langerbeck <98alla02@gmail.com>
2022-09-29 10:21:27 +0200

    Add completion lab4:18

--- edap10-labs-main/crack/src/CodeBreaker.java
+++ edap10-labs-main/crack/src/CodeBreaker.java
@@ -1,4 +1,6 @@
 import java.math.BigInteger;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -6,6 +8,7 @@ import javax.swing.JProgressBar;
 import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
 
+import client.view.ProgressItem;
 import client.view.StatusWindow;
 import client.view.WorklistItem;
 import network.Sniffer;
@@ -18,21 +21,21 @@ public class CodeBreaker implements SnifferCallback {
 
     private final JPanel workList;
     private final JPanel progressList;
-    
+
     private final JProgressBar mainProgressBar;
-
+    private static ExecutorService pool;
     // -----------------------------------------------------------------------
-    
-    private CodeBreaker() {
-        StatusWindow w  = new StatusWindow();
 
-        workList        = w.getWorkList();
-        progressList    = w.getProgressList();
+    private CodeBreaker() {
+        StatusWindow w = new StatusWindow();
+
+        workList = w.getWorkList();
+        progressList = w.getProgressList();
         mainProgressBar = w.getProgressBar();
     }
-    
+
     // -----------------------------------------------------------------------
-    
+
     public static void main(String[] args) {
 
         /*
@@ -41,7 +44,7 @@ public class CodeBreaker implements SnifferCallback {
          * 
          * That's what SwingUtilities.invokeLater is for.
          */
-
+        pool = Executors.newFixedThreadPool(2);
         SwingUtilities.invokeLater(() -> {
             CodeBreaker codeBreaker = new CodeBreaker();
             new Sniffer(codeBreaker).start();
@@ -56,39 +59,65 @@ public class CodeBreaker implements SnifferCallback {
         System.out.println("message intercepted (N=" + n + ")...");
         SwingUtilities.invokeLater(() -> {
             WorklistItem item = new WorklistItem(n, message);
-            workList.add(item);
-            
+            ProgressItem prog_aight = new ProgressItem(n, message);
             JButton btn = new JButton("Crack onnnn!!!");
+            btn.addActionListener(e -> {
+                SwingUtilities.invokeLater(() -> {
+                    workList.remove(item);
+                    progressList.add(prog_aight);
+                    ProgressTracker tracker = new Tracker(prog_aight);
+
+                    Runnable task = () -> {
+                        try {
+                            SwingUtilities.invokeLater(() -> {
+                                int progress = mainProgressBar.getValue();
+                                int maximum = mainProgressBar.getMaximum();
+                                mainProgressBar.setMaximum(maximum + 1000000);
+                                mainProgressBar.setValue(maximum + 1000000);
+                            });
+                            String plaintext = Factorizer.crack(message, n, tracker);
+                            SwingUtilities.invokeLater(() -> {
+                                JButton delete_btn = new JButton("Delete");
+                                delete_btn.addActionListener(x -> {
+                                    progressList.remove(prog_aight);
+                                });
+                                prog_aight.getTextArea().setText(plaintext);
+                                prog_aight.add(delete_btn);
+                            });
+                        } catch (InterruptedException ex) {
+                            throw new Error(ex);
+                        }
+
+                    };
+                    pool.submit(task);
+                });
+            });
+            workList.add(item);
             item.add(btn);
-            btn.addActionListener(e ->{
-                workList.remove(item);
-                progressList.add(item);
-
-                
-                ProgressTracker tracker = new Tracker();
-                try{
-                String plaintext = Factorizer.crack(message, n, tracker);
-                }catch(InterruptedException er){
-                    throw new Error(er);
-                }
-            });   
         });
-
     }
 
     private static class Tracker implements ProgressTracker {
         private int totalProgress = 0;
+        private ProgressItem prog_item;
+
+        public Tracker(ProgressItem aight) {
+            prog_item = aight;
+        }
 
         /**
          * Called by Factorizer to indicate progress. The total sum of
          * ppmDelta from all calls will add upp to 1000000 (one million).
          * 
-         * @param  ppmDelta   portion of work done since last call,
-         *                    measured in ppm (parts per million)
+         * @param ppmDelta portion of work done since last call,
+         *                 measured in ppm (parts per million)
          */
         @Override
         public void onProgress(int ppmDelta) {
             totalProgress += ppmDelta;
+            SwingUtilities.invokeLater(() -> {
+                prog_item.getProgressBar().setValue(totalProgress);
+            });
             System.out.println("progress = " + totalProgress + "/1000000");
         }
     }
